28bdf5968ec177649202e911fa31e28adb8a655b
Lizhe <Lizhe@Lizhe-PC>
2018-02-06 17:36:07 +0800

    move header to js

--- app/modules/login/Login.js
+++ app/modules/login/Login.js
@@ -3,33 +3,53 @@
  */
 import ReactDOM from 'react-dom';
 
+
+var HeaderDiv = React.createClass({
+	render: function() {
+		return (
+				
+			<div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+			    <h5 className="my-0 mr-md-auto font-weight-normal">Lira</h5>
+			    <nav className="my-2 my-md-0 mr-md-3">
+			      <a className="p-2 text-dark" href="#">Features</a>
+			      <a className="p-2 text-dark" href="#">Enterprise</a>
+			      <a className="p-2 text-dark" href="#">Support</a>
+			      <a className="p-2 text-dark" href="#">Pricing</a>
+			    </nav>
+			    <a className="btn btn-outline-primary" href="#">Sign up</a>
+			</div>
+			
+		)
+	}
+	
+});
+
 var LoginDiv = React.createClass({
-	getInitialState: function() {
-		return {value: 'Hello Runoob!'};
-	},
-		login: function(event) {
+	login: function(event) {
 		alert(123);
-				},
-				render: function() {
-					var value = this.state.value;
-					return (
+	},
+	render: function() {
+		return (
 
-						<form className="form-signin">
-							<img src={'/public/imgs/Lira.jpg'} alt="" width="72" height="72"/>
-							<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-							<label htmlFor="inputEmail" className="sr-only">Email address</label> 
-							<input type="text" id="inputUserName" className="form-control" placeholder="User Name" required autoFocus />
-							<label htmlFor="inputPassword" className="sr-only">Password</label>  
-							<input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-							<button id="subButton" className="btn btn-lg btn-primary btn-block" type="button" onClick={this.login}>Sign in</button>
-							<p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-						</form>
+			<form className="form-signin">
+				<img src={'/public/imgs/Lira.jpg'} alt="" width="72" height="72"/>
+				<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+				<label htmlFor="inputEmail" className="sr-only">Email address</label> 
+				<input type="text" id="inputUserName" className="form-control" placeholder="User Name" required autoFocus />
+				<label htmlFor="inputPassword" className="sr-only">Password</label>  
+				<input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+				<button id="subButton" className="btn btn-lg btn-primary btn-block" type="button" onClick={this.login}>Sign in</button>
+				<p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+			</form>
 
-
-					)
-				}
-			});
-    		ReactDOM.render(
-      			<LoginDiv />,
-      			document.getElementById('center')
-    		);
+		)
+	}
+});
+ReactDOM.render(
+	<HeaderDiv />,
+	document.getElementById('headerDiv')
+);
+ReactDOM.render(
+	<LoginDiv />,
+	document.getElementById('centerDiv')
+);
--- build/Lira.js
+++ build/Lira.js
@@ -946,55 +946,98 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var LoginDiv = React.createClass({
-	displayName: 'LoginDiv',
+var HeaderDiv = React.createClass({
+	displayName: "HeaderDiv",
 
-	getInitialState: function getInitialState() {
-		return { value: 'Hello Runoob!' };
-	},
-	login: function login(event) {
-		alert(123);
-	},
 	render: function render() {
-		var value = this.state.value;
 		return React.createElement(
-			'form',
-			{ className: 'form-signin' },
-			React.createElement('img', { src: '/public/imgs/Lira.jpg', alt: '', width: '72', height: '72' }),
+			"div",
+			{ className: "d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow" },
 			React.createElement(
-				'h1',
-				{ className: 'h3 mb-3 font-weight-normal' },
-				'Please sign in'
+				"h5",
+				{ className: "my-0 mr-md-auto font-weight-normal" },
+				"Lira"
 			),
 			React.createElement(
-				'label',
-				{ htmlFor: 'inputEmail', className: 'sr-only' },
-				'Email address'
+				"nav",
+				{ className: "my-2 my-md-0 mr-md-3" },
+				React.createElement(
+					"a",
+					{ className: "p-2 text-dark", href: "#" },
+					"Features"
+				),
+				React.createElement(
+					"a",
+					{ className: "p-2 text-dark", href: "#" },
+					"Enterprise"
+				),
+				React.createElement(
+					"a",
+					{ className: "p-2 text-dark", href: "#" },
+					"Support"
+				),
+				React.createElement(
+					"a",
+					{ className: "p-2 text-dark", href: "#" },
+					"Pricing"
+				)
 			),
-			React.createElement('input', { type: 'text', id: 'inputUserName', className: 'form-control', placeholder: 'User Name', required: true, autoFocus: true }),
 			React.createElement(
-				'label',
-				{ htmlFor: 'inputPassword', className: 'sr-only' },
-				'Password'
-			),
-			React.createElement('input', { type: 'password', id: 'inputPassword', className: 'form-control', placeholder: 'Password', required: true }),
-			React.createElement(
-				'button',
-				{ id: 'subButton', className: 'btn btn-lg btn-primary btn-block', type: 'button', onClick: this.login },
-				'Sign in'
-			),
-			React.createElement(
-				'p',
-				{ className: 'mt-5 mb-3 text-muted' },
-				'\xA9 2017-2018'
+				"a",
+				{ className: "btn btn-outline-primary", href: "#" },
+				"Sign up"
 			)
 		);
 	}
+
 }); /**
      * 
      */
 
-_reactDom2.default.render(React.createElement(LoginDiv, null), document.getElementById('center'));
+
+var LoginDiv = React.createClass({
+	displayName: "LoginDiv",
+
+	login: function login(event) {
+		alert(123);
+	},
+	render: function render() {
+		return React.createElement(
+			"form",
+			{ className: "form-signin" },
+			React.createElement("img", { src: '/public/imgs/Lira.jpg', alt: "", width: "72", height: "72" }),
+			React.createElement(
+				"h1",
+				{ className: "h3 mb-3 font-weight-normal" },
+				"Please sign in"
+			),
+			React.createElement(
+				"label",
+				{ htmlFor: "inputEmail", className: "sr-only" },
+				"Email address"
+			),
+			React.createElement("input", { type: "text", id: "inputUserName", className: "form-control", placeholder: "User Name", required: true, autoFocus: true }),
+			React.createElement(
+				"label",
+				{ htmlFor: "inputPassword", className: "sr-only" },
+				"Password"
+			),
+			React.createElement("input", { type: "password", id: "inputPassword", className: "form-control", placeholder: "Password", required: true }),
+			React.createElement(
+				"button",
+				{ id: "subButton", className: "btn btn-lg btn-primary btn-block", type: "button", onClick: this.login },
+				"Sign in"
+			),
+			React.createElement(
+				"p",
+				{ className: "mt-5 mb-3 text-muted" },
+				"\xA9 2017-2018"
+			)
+		);
+	}
+});
+_reactDom2.default.render(React.createElement(HeaderDiv, null), document.getElementById('headerDiv'));
+_reactDom2.default.render(React.createElement(LoginDiv, null), document.getElementById('centerDiv'));
 
 /***/ }),
 /* 15 */
