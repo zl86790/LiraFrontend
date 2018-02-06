60955c071141ca7cdbb6de0bc25f896196f3c85f
Lizhe <Lizhe@Lizhe-PC>
2018-02-06 17:15:43 +0800

    fixed htmlFor and autoFocus

--- app/modules/login/Login.js
+++ app/modules/login/Login.js
@@ -1,6 +1,7 @@
 /**
  * 
  */
+import ReactDOM from 'react-dom';
 
 var LoginDiv = React.createClass({
 	getInitialState: function() {
@@ -16,9 +17,9 @@ var LoginDiv = React.createClass({
 						<form className="form-signin">
 							<img src={'/public/imgs/Lira.jpg'} alt="" width="72" height="72"/>
 							<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-							<label for="inputEmail" className="sr-only">Email address</label> 
-							<input type="text" id="inputUserName" className="form-control" placeholder="User Name" required autofocus />
-							<label for="inputPassword" className="sr-only">Password</label>  
+							<label htmlFor="inputEmail" className="sr-only">Email address</label> 
+							<input type="text" id="inputUserName" className="form-control" placeholder="User Name" required autoFocus />
+							<label htmlFor="inputPassword" className="sr-only">Password</label>  
 							<input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
 							<button id="subButton" className="btn btn-lg btn-primary btn-block" type="button" onClick={this.login}>Sign in</button>
 							<p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
