a2c68ce5dcef4cce78abea3e436ada28b2a5eeab
Lizhe <Lizhe@Lizhe-PC>
2018-02-06 16:58:12 +0800

    added login page with alert

---
+++ app/modules/login/Login.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+var LoginDiv = React.createClass({
+	getInitialState: function() {
+		return {value: 'Hello Runoob!'};
+	},
+		login: function(event) {
+		alert(123);
+				},
+				render: function() {
+					var value = this.state.value;
+					return (
+
+						<form className="form-signin">
+							<img src={'/public/imgs/Lira.jpg'} alt="" width="72" height="72"/>
+							<h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+							<label for="inputEmail" className="sr-only">Email address</label> 
+							<input type="text" id="inputUserName" className="form-control" placeholder="User Name" required autofocus />
+							<label for="inputPassword" className="sr-only">Password</label>  
+							<input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+							<button id="subButton" className="btn btn-lg btn-primary btn-block" type="button" onClick={this.login}>Sign in</button>
+							<p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+						</form>
+
+
+					)
+				}
+			});
+    		ReactDOM.render(
+      			<LoginDiv />,
+      			document.getElementById('center')
+    		);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 module.exports = {
-  entry: './app/index.js',
+  entry:  {
+	    login: './app/modules/login/login.js'
+  },
   output: {
       path: __dirname + '/build',
       filename: "Lira.js"
